9c5c35c953abadf406d9f640a49b4826e74d416f
Fardeen Khan <khanfardeen9983@gmail.com>
2020-04-19 03:12:41 +0530

    [KotlinBeginnnerPath] : FAB change Value

--- KotinBeginnerPath/app/src/main/java/com/pluralsight/kotlin/notekeeper/MainActivity.kt
+++ KotinBeginnerPath/app/src/main/java/com/pluralsight/kotlin/notekeeper/MainActivity.kt
@@ -2,11 +2,22 @@ package com.pluralsight.kotlin.notekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            val originalValue : Int = textDisplayedValue.text.toString().toInt()
+            val newValue = originalValue*2
+            textDisplayedValue.text = newValue.toString()
+            Snackbar.make(it,"Value $originalValue changed to $newValue",Snackbar.LENGTH_LONG).show()
+        }
+
+
     }
 }
